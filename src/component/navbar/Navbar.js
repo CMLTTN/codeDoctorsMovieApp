18744cea3ea688ccc70d5c54ff06a5873fa76736
CMLTTN <cemaldim@gmail.com>
2023-03-25 17:03:23 +0300

    navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,42 +1,40 @@
-import { Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate} from 'react-router-dom'
 import "./Navbar.css";
-import { useLocation } from "react-router-dom";
-import { useContext, useState } from "react";
-import { AuthContext } from "../../context/AuthContext";
+import { useLocation } from 'react-router-dom';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
 
-const Navbar = () => {
-  let path = useLocation().pathname;
-  const {
-    currentUser,
-    currentUser: { displayName },
-  } = useContext(AuthContext);
-
-  console.log(displayName);
-  const navigate = useNavigate();
-
-  const signOutFunc = () => {
-    navigate("/register");
-  };
-  return (
-    <div className="navbar">
-      <div className="title">
-        <Link
-          style={{ textDecoration: "none" }}
-          to={`${path === "/" ? "/comments" : "/"}`}
-        >
-          <div className="h1">
-            Movie<span>DataBase</span>
-            {currentUser && path === "/" && <h6>Comment Page</h6>}
-            {currentUser && path === "/comments" && <h6>Home Page</h6>}
-          </div>
-        </Link>
-      </div>
-      <div className="logout">
-        <h6>{displayName}</h6>
-        <button onClick={signOutFunc}>Logout</button>
-      </div>
-    </div>
-  );
+ const Navbar = () => {
+   const path = useLocation().pathname;
+   const {
+     currentUser,
+     currentUser: { displayName },
+   } = useContext(AuthContext);
+   const navigate = useNavigate()
+  //  const displayName = "Cemalettin";
+const signOutFunc =()=>{
+navigate("/register");
 };
+   return (
+     <div className="navbar">
+       <div className="title">
+         <Link
+           style={{ textDecoration: "none" }}
+           to={`${path === "/" ? "/comments" : "/"}`}
+         >
+           {/* <div className="h1">
+             Movie<span>DataBase</span>
+             {currentUser && path === "/" && <h6>Comment Page</h6>}
+             {currentUser && path === "/comments" && <h6>Home Page</h6>}
+           </div> */}
+         </Link>
+       </div>
+       <div className="logout">
+         <h6>{displayName}</h6>
+         <button onClick={signOutFunc}>Logout</button>
+       </div>
+     </div>
+   );
+ };
 
-export default Navbar;
+ export default Navbar;
