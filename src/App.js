01c1743761648314599dec1e1a7e0741faec43f2
CMLTTN <cemaldim@gmail.com>
2023-03-25 17:31:43 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
  import "./App.css";
  import AuthContextProvider from "./context/AuthContext";
+ import MovieContextProvider from "./context/MovieContext";
  import AppRouter from "./router/AppRouter";
 
  function App() {
    return (
      <AuthContextProvider>
-       <AppRouter />
+       <MovieContextProvider>
+         <AppRouter />
+       </MovieContextProvider>
      </AuthContextProvider>
    );
  }
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,40 +1,45 @@
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import { useLocation } from 'react-router-dom';
-import { useContext, useState } from 'react';
-import { AuthContext } from '../../context/AuthContext';
+import { useLocation } from "react-router-dom";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { signOut } from "firebase/auth";
+import { auth } from "../../auth/firebase-config";
 
- const Navbar = () => {
-   const path = useLocation().pathname;
-   const {
-     currentUser,
-     currentUser: { displayName },
-   } = useContext(AuthContext);
-   const navigate = useNavigate()
-  //  const displayName = "Cemalettin";
-const signOutFunc =()=>{
-navigate("/register");
+const Navbar = () => {
+  let path = useLocation().pathname;
+  const { currentUser } = useContext(AuthContext);
+
+  console.log(currentUser);
+  const navigate = useNavigate();
+
+  const signOutFunc = async () => {
+    await signOut(auth);
+    navigate("/");
+  };
+  return (
+    <div className="navbar">
+      <div className="title">
+        <Link
+          style={{ textDecoration: "none" }}
+          to={`${path === "/" ? "/comments" : "/"}`}
+        >
+          <div className="h1">
+            Movie<span>DataBase</span>
+            {currentUser && path === "/" && <h6>Comment Page</h6>}
+            {currentUser && path === "/comments" && <h6>Home Page</h6>}
+          </div>
+        </Link>
+      </div>
+      {currentUser ? (
+        <div className="logout">
+          <h6>{currentUser.displayName}</h6>
+
+          {currentUser && <button onClick={signOutFunc}>Logout</button>}
+        </div>
+      ) : null}
+    </div>
+  );
 };
-   return (
-     <div className="navbar">
-       <div className="title">
-         <Link
-           style={{ textDecoration: "none" }}
-           to={`${path === "/" ? "/comments" : "/"}`}
-         >
-           <div className="h1">
-             Movie<span>DataBase</span>
-             {currentUser && path === "/" && <h6>Comment Page</h6>}
-             {currentUser && path === "/comments" && <h6>Home Page</h6>}
-           </div>
-         </Link>
-       </div>
-       <div className="logout">
-         <h6>{displayName}</h6>
-         <button onClick={signOutFunc}>Logout</button>
-       </div>
-     </div>
-   );
- };
 
- export default Navbar;
+export default Navbar;
